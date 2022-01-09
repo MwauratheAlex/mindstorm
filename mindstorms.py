bea83634bac9c3e89d31519964e3e40a2193cad4
MwauratheAlex <mwaurathealex@gmail.com>
2022-01-09 04:46:09 +0300

    add mindstorm.py

---
+++ mindstorms.py
@@ -0,0 +1,33 @@
+import turtle
+
+def draw_square(some_turtle):
+	for i in range(4):
+		some_turtle.forward(100)
+		some_turtle.right(90)
+
+def draw_art():
+	window = turtle.Screen()
+	window.bgcolor("cyan")
+
+	#create turtle brad - Draws square
+	brad = turtle.Turtle()
+	brad.shape("turtle")
+	brad.color("blue")
+	brad.speed(2)
+
+	for i in range(36):
+		draw_square(brad)
+		brad.right(10)
+		
+		
+
+	#create turtle angie - Draws circle
+	#angie = turtle.Turtle()
+	#angie.shape("arrow")
+	#angie.color("blue")
+	#angie.circle(100)
+
+	window.exitonclick()
+
+draw_art()
+
